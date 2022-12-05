4ad9f13964354d407f15d6fa796cb731657b20a0
berezandiana <119860630+berezandiana@users.noreply.github.com>
2022-12-05 14:11:54 +0200

    Update Source1.cpp

--- Source1.cpp
+++ Source1.cpp
@@ -32,8 +32,9 @@ int main()
 	int arr[N][N] = { { 1, 2, 3, 4 },
 					{ 5, 6, 7, 8 },
 					{ 9, 10, 11, 12 },
-					{ 13, 14, 15, 16 } };
+					{ 22, 14, 15, 16 } };
 	rotate90Clockwise(arr);
 	printMatrix(arr);
 	return 0;
 }
+
